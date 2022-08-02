da7452964d19d04dd1c0b69276f2b675f2729bab
Alexander Berndt <al-berndt@gmx.de>
2022-08-02 14:58:48 +0200

    added tests and fixed selectPathFromStack crashing when state is undefined

--- packages/got-core/src/index.js
+++ packages/got-core/src/index.js
@@ -224,6 +224,11 @@ export const pickMapGraph = R.curry((fnMap, view, graph) => {
 export const filterGraph = pickMapGraph(R.identity);
 
 export const selectPathFromStack = R.curry((path, stack, fnMergeLeft, state) => {
+    if (!path || !stack || !fnMergeLeft || !state) {
+        console.log('returning undefined');
+        return undefined;
+    }
+
     let acc;
     stack.forEach(graphName => {
         const val = getPath([graphName, ...path], state);
