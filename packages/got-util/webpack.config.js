5feb03d2f370c7c53bedc4a66fa559c8ae0d710c
Berndy <al-berndt@gmx.de>
2023-11-24 15:43:24 +0100

    webpack config for util to not minify

--- packages/got-util/webpack.config.js
+++ packages/got-util/webpack.config.js
@@ -1,15 +1,45 @@
 const path = require('path');
+const fs = require('fs');
+
+// Function to recursively get all files in a directory
+function getAllFiles(dirPath, arrayOfFiles = []) {
+    const files = fs.readdirSync(dirPath);
+
+    files.forEach((file) => {
+        if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+            arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles);
+        } else if (file.endsWith('.js') && !file.endsWith('.spec.js')) {
+            arrayOfFiles.push(path.join(dirPath, '/', file));
+        }
+    });
+
+    return arrayOfFiles;
+}
+
+// Generate entry object dynamically
+const srcDir = './src';
+const entryFiles = getAllFiles(srcDir).reduce((acc, filePath) => {
+    const entryKey = filePath.replace(`${srcDir}/`, '').replace('.js', '');
+    acc[entryKey] = `./${filePath}`;
+    return acc;
+}, {});
+
+console.log(entryFiles);
 
 const commonConfig = {
     // Mode can be 'development' or 'production'
     mode: 'production',
 
     // Entry file of your library
-    entry: './src/index.js',
+    entry: entryFiles,
     // External dependencies that shouldn't be bundled
     externals: {
         ramda: 'ramda',
         'ramda-adjunct': 'ramda-adjunct',
+    },
+
+    optimization: {
+        minimize: false, // Disables minification
     },
 
     // Module resolution
