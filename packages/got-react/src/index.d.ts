84de8258ed3aef2431987aec7e481460315b1807
Alexander Berndt <al-berndt@gmx.de>
2024-04-09 17:44:20 +0200

    Feature/local graph (#58)
    * exported Atom type from got-atom
    
    * added createLocalGraph to got-react
    
    * [release]
    
    * v0.23.4
    
    ---------
    
    Co-authored-by: gothub-info <info@gothub.io>

--- packages/got-react/src/index.d.ts
+++ packages/got-react/src/index.d.ts
@@ -388,7 +388,7 @@ export declare interface SetupOptions {
     atom?: Atom<State>;
     /**
      * Selector hook of the current store. Can be useAtom or useSelector.
-     * 
+     *
      * @example
      * (fnSelect, fnEquals) => useSelector(fnSelect, fnEquals);
      * (fnSelect, fnEquals) => useAtom(atom, fnSelect, fnEquals);
@@ -444,3 +444,21 @@ export declare const setup: (options: SetupOptions) => CreateResult & {
  * got store.
  */
 export declare const createHooks: (options: CreateHooksOptions) => CreateResult;
+
+/**
+ * Factory function for creating a local got state and graph functions.
+ *
+ * When performing a lot of operations on the graph it is recommended to create a local graph to avoid unnecessary updates to the global store.
+ *
+ * By default the local graph will operate on the stack ['main', 'edit']. All edits made on the local graph will be stored in the "edit" graph.
+ *
+ * The local graph can be merged to the global got store by calling `mergeGraph(getGraph('edit'))`,
+ * where `getGraph` is a function returned by `createLocalGraph` and `mergeGraph` is the function relating to the global store.
+ *
+ * The local graph does not have any API capabilities.
+ */
+export declare const createLocalGraph: (initialState: State) => CreateGraphResult & {
+    store: GotStore;
+    getState: () => State;
+    getGraph: () => Graph;
+};
