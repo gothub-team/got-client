6168b688fc016735379afdbb1e9b0dd160ba5524
Vincenz Koop <vincenz.koop@gmail.com>
2024-04-18 14:38:27 +0200

    testcase for include node

--- packages/got-store/src/types/ViewResult.test.ts
+++ packages/got-store/src/types/ViewResult.test.ts
@@ -4,6 +4,7 @@ import { type TrueCases, type Expect } from 'type-testing';
 import { type ViewResult } from './ViewResult';
 
 type Extends<A, B> = A extends B ? true : false;
+type Equal<A, B> = A extends B ? (B extends A ? true : false) : false;
 
 const baseViewResultType = () => {
     const view = {
@@ -34,7 +35,52 @@ const baseViewResultType = () => {
         };
     };
 
-    type TestResult = TrueCases<
-        [Extends<ViewResult<typeof view>, TestViewResult>, Extends<TestViewResult, ViewResult<typeof view>>]
-    >;
+    type TestResult = Expect<Equal<ViewResult<typeof view>, TestViewResult>>;
+};
+
+const includeNodeLevel0 = () => {
+    const view = {
+        rootBabbeli: {
+            include: {
+                node: true,
+            },
+        },
+    } satisfies View;
+
+    type TestViewResult = {
+        rootBabbeli: {
+            nodeId: string;
+            node: Node;
+        };
+    };
+
+    type TestResult = Expect<Equal<ViewResult<typeof view>, TestViewResult>>;
+};
+
+const includeNodeLevel1 = () => {
+    const view = {
+        rootBabbeli: {
+            edges: {
+                'root/member': {
+                    include: {
+                        node: true,
+                    },
+                },
+            },
+        },
+    } satisfies View;
+
+    type TestViewResult = {
+        rootBabbeli: {
+            nodeId: string;
+            'root/member': {
+                [id: string]: {
+                    nodeId: string;
+                    node: Node;
+                };
+            };
+        };
+    };
+
+    type TestResult = Expect<Equal<ViewResult<typeof view>, TestViewResult>>;
 };
